e2bb54c60a27f228a43f8f3ede4e0b1b7adf8a22
Erhan2000 <erhan-kurnaz@hotmail.com>
2020-04-04 19:09:07 +0200

    made basic form

--- UrenTijd/MainWindow.xaml.cs
+++ UrenTijd/MainWindow.xaml.cs
@@ -1,28 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using UrenTijd.ui;
 
 namespace UrenTijd
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public struct TimeWindow
+        {
+            public System.DateTime? from;
+            public System.DateTime? until;
+        }
+
+        public struct DayFieldsStruct
+        {
+            public TimeWindow arriving;
+            public TimeWindow leaving;
+            public TimeWindow working;
+            public string workDescription;
+            public string workType;
+            public bool worked;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private DayFieldsStruct ConvertToDayFields(DayFields dayFields)
+        {
+            DayFieldsStruct dayFieldsStruct = new DayFieldsStruct();
+
+            dayFieldsStruct.arriving.from = dayFields.GoFrom.SelectedTime;
+            dayFieldsStruct.arriving.until = dayFields.GoUntil.SelectedTime;
+
+            dayFieldsStruct.leaving.from = dayFields.BackFrom.SelectedTime;
+            dayFieldsStruct.leaving.until = dayFields.BackUntil.SelectedTime;
+
+            dayFieldsStruct.working.from = dayFields.WorkFrom.SelectedTime;
+            dayFieldsStruct.working.until = dayFields.WorkUntil.SelectedTime;
+
+            dayFieldsStruct.workDescription = dayFields.WorkDescription.Text;
+            dayFieldsStruct.workType = dayFields.WorkType.Text;
+            dayFieldsStruct.worked = dayFields.worked.IsEnabled;
+
+            return dayFieldsStruct;
+        }
+
+        private void Send(object sender, RoutedEventArgs e)
+        {
+            DayFieldsStruct[] days = new DayFieldsStruct[7];
+
+            days[0] = ConvertToDayFields(Monday);
+            days[1] = ConvertToDayFields(Tuesday);
+            days[2] = ConvertToDayFields(Wednesday);
+            days[3] = ConvertToDayFields(Thursday);
+            days[4] = ConvertToDayFields(Friday);
+            days[5] = ConvertToDayFields(Saturday);
+            days[6] = ConvertToDayFields(Sunday);
         }
     }
 }
---
+++ UrenTijd/ui/DayFields.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UrenTijd.ui
+{
+    /// <summary>
+    /// Interaction logic for DayFields.xaml
+    /// </summary>
+    public partial class DayFields : UserControl
+    {
+        public static readonly DependencyProperty DayNameProperty = DependencyProperty.Register("DayName", typeof(string), typeof(UserControl), new PropertyMetadata(""));
+        public string DayName { get { return (string)GetValue(DayNameProperty); } set { SetValue(DayNameProperty, value); } }
+
+        public DayFields()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
